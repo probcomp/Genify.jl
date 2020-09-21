6c5aa89df8f212f47e4ac426cf27c02f408dd95e
Xuan <tanqazx@gmail.com>
2020-09-20 23:50:59 -0400

    Include type signature in generated function.

--- src/Genify.jl
+++ src/Genify.jl
@@ -64,18 +64,19 @@ function genify(fn::Function, arg_types...;
     # Construct and transform IR
     ir = IR(typeof(fn), arg_types...; slots=true)
     if isnothing(ir) error("No IR available for this method signature.") end
+    arg_types = collect(ir.meta.method.sig.parameters)[2:end]
     if verbose println("== Original IR =="); display(ir) end
     ir = genify_ir(ir; autoname=autoname)
     if verbose println("== Transformed IR =="); display(ir) end
     # Build new Julia function
-    traced_fn = build_func(ir, gensym(fn_name))
+    traced_fn = build_func(ir, gensym(fn_name), [Any; arg_types])
     if !return_gf return traced_fn end
     # Embed Julia function within dynamic generative function
     arg_defaults = fill(nothing, n_args)
     has_argument_grads = fill(false, n_args)
     accepts_output_grad = false
     gen_fn = Gen.DynamicDSLFunction{Any}(
-        Dict(), Dict(), collect(arg_types), false, arg_defaults,
+        Dict(), Dict(), arg_types, false, arg_defaults,
         traced_fn, has_argument_grads, accepts_output_grad)
     return gen_fn
 end
@@ -108,10 +109,11 @@ function genify_ir(ir::IR; autoname::Bool=true)
 end
 
 "Build Julia function from IR."
-function build_func(ir::IR, name::Symbol)
+function build_func(ir::IR, name::Symbol, types=nothing)
     argnames = [Symbol(:arg, i) for i=1:length(arguments(ir))]
-    # TODO: Add type annotations
-    fn = @eval @generated function $(name)($(argnames...))
+    if isnothing(types) types = [Any for i=1:length(arguments(ir))] end
+    argsigs = [Expr(:(::), n, QuoteNode(t)) for (n, t) in zip(argnames, types)]
+    fn = @eval @generated function $(name)($(argsigs...))
         return IRTools.Inner.build_codeinfo($ir)
     end
     return fn
