bcf1bf91dc12b04ea4e663e896a2b7d68c8db241
Xuan <tanqazx@gmail.com>
2020-10-16 21:08:17 -0400

    Fixes to data-driven SMC with transmission counts.

--- examples/sir/inference/data_driven_smc.jl
+++ examples/sir/inference/data_driven_smc.jl
@@ -1,25 +1,43 @@
 ## Data-driven SMC algorithm
-# Data-driven Poisson regeneration proposal
-# Gaussian drift rejuvenation of parameters
 
-@gen function poisson_prop(trace, t::Int, λ::Int)
-    {:step => t => :agents => :agent_step! => :transmit! => :n} ~ poisson(λ)
+@gen function step_proposal(prev_trace, t, newly_infected)
+    model = get_retval(prev_trace)
+    n_prev_undetected = sum(a.days_infected < model.infection_period &&
+                            a.status == :I for a in values(model.agents))
+    if newly_infected <= 0 || n_prev_undetected <= 0 return end
+    # Compute average newly infected per previously infected agent
+    β = newly_infected / n_prev_undetected
+    fitted_model = deepcopy(model)
+    fitted_model.β_det .= β
+    {:step => t => :agents} ~ abm_step!(fitted_model, agent_step!, 1, true)
+    return nothing
 end
 
-function data_driven_smc(T::Int, 
-                         observations::ChoiceMap, 
-                         n_particles::Int; 
-                         drift::Bool = false,
-                         uptick::Float64 = 7.0)
+# Gets total observed infection counts for all timesteps from 1 to T
+get_infected(obs::ChoiceMap, model::ABM, T::Int) =
+    [sum(obs[:step=>t=>:obs=>:infected=>c] for c in 1:model.C) for t in 1:T]
+
+get_recovered(obs::ChoiceMap, model::ABM, T::Int) =
+    [sum(obs[:step=>t=>:obs=>:recovered=>c] for c in 1:model.C) for t in 1:T]
+
+get_susceptible(obs::ChoiceMap, model::ABM, T::Int) = nagents(model) .-
+    (get_infected(obs, model, T) .+ get_recovered(obs, model, T))
+
+function moving_average(vs, n)
+    avgs = [sum(@view vs[i:(i+n-1)])/n for i in 1:(length(vs)-(n-1))]
+    return [avgs; fill(avgs[end], n-1)]
+end
+
+function data_driven_smc(T::Int, observations::ChoiceMap, n_particles::Int;
+                         drift::Bool = false, obs_noise::Float64=5.0)
     # Initialize filter
-    noise = 5.0
-    state = pf_initialize(bayesian_sir, (0, noise), choicemap(), n_particles)
-    # Compute number of infected per time step.
-    max_num_infected_per_time = [to_array(get_selected(observations, Gen.select(:step => t => :obs => :infected)), Float64) |> maximum for t in 1 : T]
-    Δ_max_num_infected_per_time = max_num_infected_per_time - append!(Float64[0.0], max_num_infected_per_time[1 : end - 1])
-    println("Δ max of infected per time:\n$(Δ_max_num_infected_per_time)")
-    # Step through filter
-    println("Running data-driven particle filter with targeted Poisson proposal...")
+    state = pf_initialize(bayesian_sir, (0, obs_noise), choicemap(), n_particles)
+    model = get_retval(state.traces[1])
+    # Compute number of newly infected per person per time step.
+    susceptible = get_susceptible(observations, model, T) .|> round
+    newly_infected = - diff([nagents(model) - sum(model.Is); susceptible])
+    newly_infected = moving_average(newly_infected, 5)
+    println("Running SMC with data-driven transmission count proposal...")
     @showprogress for t=1:T
         obs = get_selected(observations, Gen.select(:step => t))
         # Apply drift rejuvenation.
@@ -27,16 +45,9 @@ function data_driven_smc(T::Int,
             pf_resample!(state, :residual)
             pf_rejuvenate!(state, mh, (gaussian_drift, ()))
         end
-        # Check num infected - perform proposal step for Poisson in transmit!
-        # TODO: Proposal needs to propose to more than just Poisson? For some reason I thought this was required.
-        if Δ_max_num_infected_per_time[t] > uptick
-            λ = Int(floor(Δ_max_num_infected_per_time[t]))
-            pf_update!(state, (t, noise), (UnknownChange(), NoChange()), 
-                       obs, poisson_prop, (t, λ))
-        else
-            pf_update!(state, (t, noise), (UnknownChange(), NoChange()), 
-                       obs)
-        end
+        # Upsample transmission counts based on number newly infected
+        pf_update!(state, (t, obs_noise), (UnknownChange(), NoChange()),
+                   obs, step_proposal, (t, newly_infected[t]))
     end
     # Return particles and log ML estimate
     return state
